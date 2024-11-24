4c0d35e6aa3a8a241f436f67e7ea6929eb7355e1
SupakornDPU <sup.dongkew@gmail.com>
2024-11-24 21:39:46 +0700

    Revert "film commit"
    This reverts commit b5aa3e78e32a3c5db11cc1dde69caba7692afd34.

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Login from './Login.jsx'
-
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />
-  }
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
